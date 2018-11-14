e68303e6de5fce2e85c6599385f9141b04461450
SahrulSidik <ss.sahrulsidik@gmail.com>
2018-11-14 17:27:25 +0700

    Duty Day-4

---
+++ Day0004/Pseudocode.js
@@ -0,0 +1,12 @@
+varkecepatan = 20;
+var penumpang = 4;
+
+if (penumpang > 2) {
+  kecepatan -= 5;
+} else if (penumpang === 4) {
+  Kecepatan -= 7;
+} else {
+  kecepatan -= 2;
+}
+
+console.log(kecepatan);
